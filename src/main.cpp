001afb78e660495e0c9e19de6f8091bc0dae2a3e
sm <sm@sm>
2023-10-09 15:34:02 +0900

    1st commit

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,8 @@ RB1_500E _rb;
 void gazebo::RB1_500E::Load(physics::ModelPtr _model, sdf::ElementPtr /*_sdf*/)
 {
 
+
+
     /*
      * Loading model data and initializing the system before simulation 
      */
